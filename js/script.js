2b2d7d97e8907dab869ad946cd3ecacf18f2a249
Egor <sartvas@yandex.ru>
2021-04-15 17:26:30 +0300

    Home work Part3

--- js/script.js
+++ js/script.js
@@ -1,9 +1,5 @@
 /* Задания на урок:
 
-1) Удалить все рекламные блоки со страницы (правая часть сайта)
-
-2) Изменить жанр фильма, поменять "комедия" на "драма"
-
 3) Изменить задний фон постера с фильмом на изображение "bg.jpg". Оно лежит в папке img.
 Реализовать только при помощи JS
 
@@ -21,42 +17,38 @@ const movieDB = {
         "Ла-ла лэнд",
         "Одержимость",
         "Скотт Пилигрим против..."
-    ], 
+    ]
 };
 
-const adv = document.querySelectorAll(".promo__adv img"),
-      promoTitle = document.querySelector(".promo__adv-title"),
-      promoMenuItem = document.querySelectorAll(".promo__menu-item"),
-      poster = document.querySelector(".promo__bg"),
-      movieList = document.querySelector('.promo__interactive-list');
+const adv = document.querySelectorAll('.promo__adv img'),
+      poster = document.querySelector('.promo__bg'),
+      promoGenre = poster.querySelector('.promo__genre'),
+      menuList = document.querySelector('.promo__menu-list'),
+      interactiveList = document.querySelector('.promo__interactive-list');
       
-
-adv.forEach(item => {
+      
+adv.forEach(item =>{
     item.remove();
 });
 
-promoTitle.textContent = "";
-promoMenuItem[0].textContent = "Фильмецы";
-promoMenuItem[1].textContent = "Серияльчики";
-promoMenuItem[2].textContent = "Мультяшки";
- console.log(poster);
+promoGenre.textContent = 'драма';
 
- poster.style.backgroundImage = "url('img/bg.jpg')";
+poster.style.backgroundImage = 'url("img/bg.jpg")';
 
- movieList.innerHTML = '';
+interactiveList.innerHTML = "";
 
- movieDB.movies.sort();
+movieDB.movies.sort();
 
- movieDB.movies.forEach((films,i) => {
-        movieList.innerHTML += `
-        <li class="promo__interactive-item">${i + 1}. ${films}
-        <div class="delete"></div>
-        </li>
-        `;
- });
+movieDB.movies.forEach((item,i) =>{
+    interactiveList.innerHTML += `
+    <li class="promo__interactive-item">${i+1}) ${item}
+     <div class="delete"></div>
+     </li>
+    `;
+});
 
 
+console.dir(menuList);
 
-
-
+    
 
